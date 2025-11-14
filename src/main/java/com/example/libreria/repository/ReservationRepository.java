4d5f793b00e95996b1a4be6f613f6bc865344af7
DanaeBM-405451 <405451@tecnicatura.frc.utn.edu.ar>
2025-11-14 19:29:29 -0300

    ReservationRepository

--- src/main/java/com/example/libreria/repository/ReservationRepository.java
+++ src/main/java/com/example/libreria/repository/ReservationRepository.java
@@ -5,13 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    
+   List<Reservation> findByUserId(Long userId);
+
+   List<Reservation> findByStatus(Reservation.ReservationStatus status);
+
+  // @Query ("SELECT r FROM Reservation  r WHERE  r.expectedReturnDate < :today  AND r.status = 'ACTIVE'")
+    //List<Reservation> findOverdueReservations(@Param("today") LocalDate today); tengo miendo que esta falle pero opcion b por si no anda
+
+    @Query ("SELECT r FROM Reservation  r WHERE  r.expectedReturnDate < CURRENT_DATE AND r.status = 'ACTIVE'")
+    List<Reservation> findOverdueReservations();
+
     // TODO: Implementar los métodos de la reserva
 }
 
